e90b099a1a7b117490d31acaaaca90be6ca85ff1
Ahmed AlSum <aalsum@stanford.edu>
2015-05-05 10:53:16 -0700

    the robot can route to the right WF

--- robots/start_special_dissemination.rb
+++ robots/start_special_dissemination.rb
@@ -14,9 +14,18 @@ module Robots
         # @param [String] druid -- the Druid identifier for the object to process
         def perform(druid)
           druid_obj = Dor::Item.find(druid)
-        end
-      end
+          if druid_obj.identityMetadata.objectType == ["item"] && !druid_obj.contentMetadata.nil? then
 
+            if druid_obj.contentMetadata.contentType == ["webarchive-seed"] then
+              druid_obj.initialize_workflow('wasSeedDisseminationWF')
+            elsif druid_obj.contentMetadata.contentType == ["file"] then
+              druid_obj.initialize_workflow('wasCrawlDisseminationWF')
+            end
+
+          end
+        end
+
+      end
     end
   end
 end
