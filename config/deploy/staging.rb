5dffeb926b8308f1a2793716cb304f9d64ed56a6
Darren Hardy <drh@stanford.edu>
2016-03-01 14:25:15 -0800

    cap environments deploys

---
+++ config/deploy/staging.rb
@@ -0,0 +1,7 @@
+server 'sul-robots5-test.stanford.edu', user: 'lyberadmin', roles: %w{web app db}
+
+Capistrano::OneTimeKey.generate_one_time_key!
+
+set :deploy_environment, 'test'
+set :whenever_environment, fetch(:deploy_environment)
+set :default_env, { :robot_environment => fetch(:deploy_environment) }
